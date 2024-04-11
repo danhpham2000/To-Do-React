b6c2fc756da44cb031c7c5054cd5962141a8b514
Danh Pham <115748225+danhpham2000@users.noreply.github.com>
2024-04-10 19:04:27 -0700

    Add MCs and routes and user form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Navbar from "./components/Navbar/Navbar";
-import TodoList from "./components/ToDo/TodoList";
 import Login from "./components/User/Login";
 import Signup from "./components/User/Signup";
 import ToDoCard from "./components/ToDo/ToDoCard";
@@ -11,12 +10,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route index />
+        <Route index element={<ToDoCard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-
-      <ToDoCard />
     </Router>
   );
 }
--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import "./User.css";
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h2 className="title-login">Login</h2>
+
+        <form className="login-form">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+
+          <button className="btn-login">Login</button>
+          <div className="register">
+            New to Productive? <Link to="/signup">Sign up</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
