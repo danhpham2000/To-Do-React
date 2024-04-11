a2cd32f8310c709273f58879d07d04e8f7c3ce2a
Danh Pham <115748225+danhpham2000@users.noreply.github.com>
2024-04-10 23:56:12 -0700

    Add server and model

--- src/components/User/Signup.jsx
+++ src/components/User/Signup.jsx
@@ -1,5 +1,30 @@
+import { Link } from "react-router-dom";
+import "./User.css";
+
 const Signup = () => {
-  return <h1>Signup</h1>;
+  return (
+    <div className="login-container">
+      <div className="signup-card">
+        <h2 className="title-login">Sign up</h2>
+
+        <form className="login-form">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+
+          <label htmlFor="confirmedPassword">Confirmed Password</label>
+          <input type="password" name="confirmedPassword" />
+
+          <button className="btn-login">Sign Up</button>
+          <div className="register">
+            Already register? <Link to="/login">Login</Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
